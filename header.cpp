3710890fec931ce1d1d58087c8002ba89c68e007
hunnitt <kdhassa0804@gmail.com>
2018-09-15 04:37:56 -0700

    Add header modules

---
+++ header.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "header.h"
+
+void parse(ETH_HDR * eth_hdr, 
+          IP_HDR * ip_hdr, 
+          TCP_HDR * tcp_hdr, 
+          DATA * data, 
+          const u_char * packet) {
+    const u_char * p = packet;
+    memcpy(eth_hdr, p, sizeof(ETH_HDR));
+    p += sizeof(ETH_HDR);
+    memcpy(ip_hdr, p, sizeof(IP_HDR));
+    p += (ip_hdr->HEADER_LENGTH)*4;
+    memcpy(tcp_hdr, p, sizeof(TCP_HDR));
+    p += sizeof(TCP_HDR);
+    memcpy(data, p, sizeof(DATA));
+}
+
+void freeall(ETH_HDR * eth_hdr,
+             IP_HDR * ip_hdr,
+             TCP_HDR * tcp_hdr,
+             DATA * data) {
+    free(eth_hdr);
+    free(ip_hdr);
+    free(tcp_hdr);
+    free(data);
+}
---
+++ header.h
@@ -0,0 +1,48 @@
+#pragma once
+
+typedef struct ethernet_header {
+    u_int8_t DEST_MAC[6];
+    u_int8_t SRC_MAC[6];
+    u_int16_t TYPE;
+}ETH_HDR;
+
+typedef struct IP_header {
+#if BYTE_ORDER == LITTLE_ENDIAN
+    unsigned int HEADER_LENGTH:4;
+    unsigned int VERSION:4;
+#endif
+#if BYTE_ORDER == BIG_ENDIAN
+    unsigned int VERSION:4;
+    unsigned int HEADER_LENGTH:4;
+#endif
+    u_int8_t TYPE_OF_SERVICE;
+    u_int16_t TOTAL_LENGTH;
+    u_int16_t IDENTIFIER;
+    u_int16_t FRAGMENT_OFFSET;
+    u_int8_t TIME_TO_LIVE;
+    u_int8_t PROTOCOL_ID;
+    u_int16_t HEADER_CHECKSUM;
+    unsigned char SRC_IP[4];
+    unsigned char DEST_IP[4];
+}IP_HDR;
+
+typedef struct TCP_header {
+    u_int16_t SRC_PORT;
+    u_int16_t DEST_PORT;
+    u_int32_t SEQUENCE_NUM;
+    u_int32_t ACKNOWLEDGE_NUM;
+    unsigned int OFFSET:4;
+    unsigned int RESERVED:4;
+    u_int8_t FLAGS;
+    u_int16_t WINDOW;
+    u_int16_t CHECKSUM;
+    u_int16_t URGENT_PTR;
+}TCP_HDR;
+
+typedef struct payload {
+    unsigned char data[32];
+}DATA;
+
+void parse(ETH_HDR * eth_hdr, IP_HDR * ip_hdr, TCP_HDR * tcp_hdr, DATA * data, const u_char * packet);
+
+void freeall(ETH_HDR * eth_hdr, IP_HDR * ip_hdr,  TCP_HDR * tcp_hdr, DATA * data);

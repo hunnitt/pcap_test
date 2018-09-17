25c9715ea9598c6dcff7111ec1b4ec04dd6c4d7d
hunnitt <kdhassa0804@gmail.com>
2018-09-16 17:11:07 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,52 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-struct ethernet_header {
-    char dest_addr[6];
-    char src_addr[6];
-    char type[2];
-}ETH_HDR;
-
-struct IP_header {
-    char 
-}IP_HDR;
-
-struct TCP_header {
-    const u_char 
-}TCP_HDR;
-
-struct payload {
-    const u_char data [32];
-}DATA;
-
+#include "header.h"
 
 void usage(void) {
     printf("syntax : pcap_test <interface>\n");
     printf("sample : pcap_test ens33\n");
-}
-
-int parse(ETH_HDR * eth_hdr, 
-           IP_HDR * ip_hdr, 
-           TCP_HDR * tcp_hdr, 
-           DATA * data, 
-           const u_char * packet) {
-
-   char * p = packet;
-   
-   memcpy(eth_hdr, p, sizeof(eth_hdr));
-   p += sizeof(eth_hdr);
-
-   if(memcmp(eth_hdr->type, 0x0800)) {
-       printf("it isn't IP Datagram.\n");
-       return -3;
-   }
-
-   memcpy(ip_hdr, p, sizeof(ip_hdr);
-   p += sizeof(ip_hdr);
-
-
-   if(memcmp(ip_hdr->PROTOCOL_ID,
 }
 
 int main(int argc, char * argv[]) {
@@ -58,22 +17,22 @@ int main(int argc, char * argv[]) {
 
     char * dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t * handle = pcap_open_live(dec, BUFSIZ, 1, 1000, errbuf);
+    pcap_t * handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 
     if(handle == NULL) {
         fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-        return -1
+        return -1;
     }
 
     while(1) {
         struct pcap_pkthdr * header;
         const u_char * packet;
         int result = pcap_next_ex(handle, &header, &packet);
-        ETH_HDR * ethhdr = malloc(sizeof(ETH_HDR));
-        IP_HDR * iphdr = malloc(sizeof(IP_HDR));
-        TCP_HDR * tcphdr = malloc(sizeof(TCP_HDR));
-        DATA * payload = malloc(sizeof(DATA));
 
+        ETH_HDR * ethhdr = (ETH_HDR *)malloc(sizeof(ETH_HDR));
+        IP_HDR * iphdr = (IP_HDR *)malloc(sizeof(IP_HDR));
+        TCP_HDR * tcphdr = (TCP_HDR *)malloc(sizeof(TCP_HDR));
+        DATA * payload = (DATA *)malloc(sizeof(DATA));
 
         // 0 : packets are being read from a live capture, 
         //     and the timeout expired
@@ -81,10 +40,66 @@ int main(int argc, char * argv[]) {
         // -1 : an error occurred while reading the packet
         // -2 : there are no more packets to read from the savefile
         if(result == -1 || result == -2) break;
-        printf("%u bytes captured\n", header->caplen);
-        parse(ethhdr, iphdr, tcphdr, payload, packet);
 
-        freeall(
+        parse(ethhdr, iphdr, tcphdr, payload, packet);
+        
+        if(ethhdr->TYPE != 0x08 || iphdr->PROTOCOL_ID != 0x06) {
+            freeall(ethhdr, iphdr, tcphdr, payload);
+            continue;
+        }
+
+        printf("%u bytes captured\n", header->caplen);
+        printf("\n");
+
+        // Ethernet info Print
+        printf("ETH header info\n");
+        printf("> Source MAC : ");
+        for(int i=0; i<6; i++) {
+            printf("%02X", ethhdr->SRC_MAC[i]);
+            if(i!=5) printf(":");
+            else printf("\n");
+        }
+        printf("> Destination MAC : ");
+        for(int i=0; i<6; i++) {
+            printf("%02X", ethhdr->DEST_MAC[i]);
+            if(i!=5) printf(":");
+            else printf("\n");
+        }
+        printf("\n");
+
+        // IP info Print
+        printf("IP header info\n");
+        printf("> Source IP : ");
+        for(int i=0; i<4; i++) {
+            printf("%d", iphdr->SRC_IP[i]);
+            if(i!=3) printf(".");
+            else printf("\n");
+        }
+        printf("> Destination IP : ");
+        for(int i=0; i<4; i++) {
+            printf("%d", iphdr->DEST_IP[i]);
+            if(i!=3) printf(".");
+            else printf("\n");
+        }
+        printf("\n");
+
+        // TCP info Print
+        printf("TCP header info\n");
+        printf("> Source port number : %X\n", tcphdr->SRC_PORT);
+        printf("> Destination port number : %X\n", tcphdr->DEST_PORT);
+        printf("\n");
+
+        // payload Print
+        printf("Payload\n");
+        u_char * p = payload->data;
+        for(int i=0; i<32; i++) {
+            printf("%02X ", *p);
+            p++;
+            if((i & 0x0F) == 0x0F)
+                printf("\n");
+        }
+        printf("\n");
+        freeall(ethhdr, iphdr, tcphdr, payload); 
     }
 
     pcap_close(handle);

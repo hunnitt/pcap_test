0dd54200a7b0ba189eadcd7308fb204b89cb1961
hunnitt <kdhassa0804@gmail.com>
2018-09-19 20:49:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ int main(int argc, char * argv[]) {
     char * dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t * handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    // for debugging.
+    // pcap_t * handle = pcap_open_offline("/home/donghunkim/Desktop/tcp-port-80-test.gilgil.pcap", errbuf);
+    
 
     if(handle == NULL) {
         fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
